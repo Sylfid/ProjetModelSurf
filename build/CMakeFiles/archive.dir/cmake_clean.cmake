ff942542bcc04be6a97dfcb0c331a56fa0f4f557
Sophia Vilayvong <Sophia.Vilayvong@ensimag.grenoble-inp.fr>
2019-12-11 20:12:29 +0100

    nettoyage du dossier build

--- build/CMakeFiles/archive.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/archive"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/archive.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
