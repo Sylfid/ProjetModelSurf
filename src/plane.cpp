90e6425bf385a8c7ddb1cfd30678c5ea3101507d
Sophia Vilayvong <Sophia.Vilayvong@ensimag.grenoble-inp.fr>
2019-12-13 18:11:17 +0100

    creation de la classe plan

---
+++ src/plane.cpp
@@ -0,0 +1,31 @@
+#include "plane.h"
+
+Plane::Plane() {}
+
+Plane::Plane(const Vector3d &center, const Vector3d &normal):center(center), normal(normal){}
+
+Plane::~Plane() {}
+
+void Plane::setNormal(const Vector3d &n) {
+  normal.setX(n.getX());
+  normal.setY(n.getY());
+  normal.setZ(n.getZ());
+}
+
+void Plane::setCenter(const Vector3d &c) {
+  center.setX(c.getX());
+  center.setY(c.getY());
+  center.setZ(c.getZ());
+}
+
+Vector3d Plane::getNormal() const {
+  return normal;
+}
+
+Vector3d Plane::getCenter() const {
+  return center;
+}
+
+void Plane::display(std::ostream& str) {
+  str << "center : " << center << ", normal : " << normal;
+}
--- src/plane.h
+++ src/plane.h
@@ -1,13 +1,49 @@
 #ifndef PLANE_H
 #define PLANE_H
 
+#include "vector3d.h"
+
+/**
+  * \brief Class représentant un plan défini par sa normale et son centre
+*/
 class Plane {
 
   private:
+    Vector3d center; /*!< centre du plan */
+    Vector3d normal; /*!< normal au plan */
 
   public:
-
+    /**
+      * \brief Constructeur par défaut de la classe.
+      * \details Ce constructeur est appelé pour l'instanciation
+      *             d'un plan. Il n'effectue rien.
+    */
     Plane();
+    /**
+      * \brief Constructeur de la classe.
+      * \details Ce constructeur est appelé pour l'instanciation
+      *             d'un plan avec son centre et sa normale
+      * \param center le centre du plan
+      * \param normal la normale au plan
+    */
+    Plane(const Vector3d &center, const Vector3d &normal);
+    /**
+      * \brief Destructeur de la classe
+    */
+    ~Plane();
+
+    void setNormal(const Vector3d &normal);
+    void setCenter(const Vector3d &center);
+    Vector3d getNormal() const;
+    Vector3d getCenter() const;
+
+    /**
+      * \brief Affiche les composantes du plan
+      * \details La méthode affichera, sur le pflux passé en paramètre, les infos
+      * \param str le flux sur lequel s'affichera le résultat
+    */
+    void display(std::ostream& str);
+
 
 };
 
